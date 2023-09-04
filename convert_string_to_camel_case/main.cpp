fa135724e37917533748a35e25bb1ea61d2af083
Sharath Kumar K <sharathkumark017@gmail.com>
2023-09-04 11:39:58 +0530

    Added a test

--- convert_string_to_camel_case/main.cpp
+++ convert_string_to_camel_case/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 #include <string>
+#include <map>
+
+void test_func(std::string (*func)(std::string text), std::map<std::string, std::string> test_data) {
+    int i = 1;
+    for (std::map<std::string, std::string>::iterator it = test_data.begin(); it != test_data.end(); ++it) {
+        if (func(it->first) == it->second) {
+            std::cout << "Test " << i << " passed.\n";
+        }
+        else {
+            std::cout << "Test " << i << " failed!\n";
+        }
+        ++i;
+    }
+}
 
 std::string to_camel_case(std::string text) {
     if (text == "")
@@ -8,7 +22,7 @@ std::string to_camel_case(std::string text) {
     for (std::string::size_type i = 0; i < text.length(); ++i) {
        if (text[i] == '_' || text[i] == '-') {
            if (text[i+1])
-               text.at(i+1) = std::toupper(text.at(i+1));
+               text.at(i+1) = toupper(text.at(i+1));
             text.erase(i, 1);
        }
     }
@@ -17,13 +31,13 @@ std::string to_camel_case(std::string text) {
 
 
 int main(void) {
-    std::string s1("the-stealth-warrior");
-    std::string s2("The_Stealth_Warrior");
-    std::string s3("The_Stealth-Warrior");
+    std::map<std::string, std::string> data = {
+        {"the-stealth-warrior", "theStealthWarrior"},
+        {"The_Stealth_Warrior", "TheStealthWarrior"},
+        {"The_Stealth-Warrior", "TheStealthWarrior"},
+    };
 
-    std::cout << "String 1 " << s1 << " -> " << to_camel_case(s1) << std::endl;
-    std::cout << "String 2 " << s2 << " -> " << to_camel_case(s2) << std::endl;
-    std::cout << "String 3 " << s3 << " -> " << to_camel_case(s3) << std::endl;
+    test_func(to_camel_case, data);
     return 0;
 }
 
